310747c9661134b0604201af71a12d9597a67295
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-02 16:15:34 +0200

    Implemented the `Concerts` class
    with a constructor that initializes title, date, seats, time, and pricing

--- src/org/eventi/java/Concerts.java
+++ src/org/eventi/java/Concerts.java
@@ -3,3 +3,16 @@ package org.eventi.java;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+public class Concerts extends Event
+{
+	private LocalTime time;
+	private float pricing;	
+	
+	Concerts (String title, LocalDate data, int totSeats, int bookedSeats, LocalTime time, float pricing)
+	{
+		super (title, data, totSeats, bookedSeats);
+		this.time = time;
+		this.pricing = pricing;
+	}
+
+}
