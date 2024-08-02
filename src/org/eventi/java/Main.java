6ebf20fcc32dda5004cc97c6dde411966d96c0eb
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-02 15:52:32 +0200

    Initialized `Scanner` and set up a basic menu loop
    for user input

--- src/org/eventi/java/Main.java
+++ src/org/eventi/java/Main.java
@@ -11,4 +11,18 @@ public class Main
 {
 	public static void main(String[] args) 
 	{	
+		Concerts userEvent = new Concerts (null, null, 0, 0, null, 0);		
+		LocalDate dataEvent = null;
+		LocalTime timeEvent = null;
+		Scanner noteDown = new Scanner(System.in); //scanner assignment
+
+		while(true) 
+		{						
+			System.out.println("\nwrite \"create\" if you want to add new event, \n"
+					+ "write \"book\" if you want to book a ticket, \n"
+					+ "write \"cancel\" if you want to cancel your booking  \n");			
+			String userBook = noteDown.nextLine();		//scanner
+			}				
+		}		
 	}
+}
