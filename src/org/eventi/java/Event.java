4756068b58a3fe7c28aaec9d8265da3ca49a146b
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-02 15:10:33 +0200

    Added: the `Event` class
    with private members `title`, `data`, `totSeats` and `bookedSeats'

---
+++ src/org/eventi/java/Event.java
@@ -0,0 +1,12 @@
+package org.eventi.java;
+
+//date management
+import java.time.LocalDate; //date organizer
+
+public class Event 
+{	
+		protected String title;
+		protected LocalDate data; 
+		protected int totSeats;
+		private int bookedSeats;		
+}
