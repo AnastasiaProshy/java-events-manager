6adbb9b39c59bf95d572d4719836492051586d48
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-02 16:02:54 +0200

    Added the `create` case skeleton
    to handle new event creation. Includes prompts for event title and date

--- src/org/eventi/java/Main.java
+++ src/org/eventi/java/Main.java
@@ -22,6 +22,19 @@ public class Main
 					+ "write \"book\" if you want to book a ticket, \n"
 					+ "write \"cancel\" if you want to cancel your booking  \n");			
 			String userBook = noteDown.nextLine();		//scanner
+			switch(userBook) 
+			{
+				//event creator
+				case "create":						
+					System.out.println("Type down new event name: ");
+					String eventTitle = noteDown.nextLine();											    
+					int i = 0;
+					while (i != 1)
+					{
+						System.out.println("Type down new event data (dd/mm/yyyy) : ");
+						String dataEventString = noteDown.nextLine();	
+					}
+					break;
 			}				
 		}		
 	}
