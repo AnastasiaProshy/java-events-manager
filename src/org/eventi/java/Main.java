cc84c90f53da8e5038880237725b4eccdf07ee4c
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-02 15:47:30 +0200

    Set up the `Main` class
    with basic imports and class structure

---
+++ src/org/eventi/java/Main.java
@@ -0,0 +1,14 @@
+package org.eventi.java;
+
+//date management
+import java.time.LocalDate;									 //date organizer
+import java.time.LocalTime; 									//time organizer
+import java.time.format.DateTimeFormatter; 		//date formatter
+
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{	
+	}
